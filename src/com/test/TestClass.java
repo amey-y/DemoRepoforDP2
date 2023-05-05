39ffc10b4bfc7e1b9f50af966034523dd350036a
AY <AY@192.168.1.47>
2023-05-05 17:56:05 +0530

    FirstCommit

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Test code for Github!");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AY
+ *
+ */
+module Java4Github {
+}
